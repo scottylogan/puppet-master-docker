de3576419521a6069150bb2fb8bb9bbfb6a6692b
Scotty Logan <scotty@catbert.net>
2016-04-25 13:03:46 -0700

    added apt-transport-https to the initial packages

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -qq update
+apt-get -qq install curl apt-transport-https
 curl -o /tmp/pl.deb https://apt.puppetlabs.com/pool/jessie/PC1/p/puppetlabs-release-pc1/puppetlabs-release-pc1_1.0.0-2jessie_all.deb
 dpkg -i /tmp/pl.deb
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
