96c08164e2070585fedb858d02ee2a98cffdcdbe
Scotty Logan <scotty@catbert.net>
2016-04-25 13:14:23 -0700

    added .aws and .ssh mounts

--- run.sh
+++ run.sh
@@ -7,6 +7,9 @@
     --name puppetmaster \
     -v $PWD/etc:/etc/puppetlabs \
     -v $PWD/log:/var/log/puppetlabs \
+    -v $PWD/aws:/opt/puppetlabs/server/data/puppetserver/.aws:ro \
+    -v $PWD/ssh:/opt/puppetlabs/server/data/puppetserver/.ssh:ro \
+    -v $PWD/ssh:/root/.ssh:ro \
     -p 8140:8140 \
     puppetmaster \
     /opt/puppetlabs/bin/puppetserver foreground \
