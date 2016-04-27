69945b95ab17cc956c6a9f9626ca711eee96bf27
Scotty Logan <swl@stanford.edu>
2016-04-26 18:06:30 -0700

    fixed volumes in run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #! /bin/sh
 
-[ `id -u` -ne 0 ] && exec sudo $0 $*
+#[ `id -u` -ne 0 ] && exec sudo $0 $*
+
+rm -rf data/puppetserver/.ssh data/puppetserver/.aws
+cp -pr ssh data/puppetserver/.ssh
+cp -pr aws data/puppetserver/.aws
 
 ( exec docker run --rm \
     -h puppet.catbert.net \
     --name puppetmaster \
     -v $PWD/etc:/etc/puppetlabs \
     -v $PWD/log:/var/log/puppetlabs \
-    -v $PWD/aws:/opt/puppetlabs/server/data/puppetserver/.aws:ro \
-    -v $PWD/ssh:/opt/puppetlabs/server/data/puppetserver/.ssh:ro \
-    -v $PWD/ssh:/root/.ssh:ro \
+    -v $PWD/data:/opt/puppetlabs/server/data \
     -p 8140:8140 \
     puppetmaster \
-    /opt/puppetlabs/bin/puppetserver foreground \
   >puppetmaster.out \
   2>puppetmaster.err & \
 ) &
